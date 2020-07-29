da528349e8555c60fc59f54576e59e7e6d9a8dbe
Hansel <hansel.hir@gmail.com>
2020-07-29 12:26:17 -0500

    feat: Se agrega implementación para el control del  tiempo de peticiones

---
+++ src/main/java/com/creativity/registrymicroservice/configuration/WebMvcConfiguration.java
@@ -0,0 +1,21 @@
+package com.creativity.registrymicroservice.configuration;
+
+import com.creativity.registrymicroservice.component.RequestTimeInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfiguration implements WebMvcConfigurer {
+
+    @Autowired
+    @Qualifier("requestTimeInterceptor")
+    private RequestTimeInterceptor requestTimeInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(requestTimeInterceptor);
+    }
+}
