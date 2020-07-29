fe1fa2c01a45698f57b779e5c2b6527f259f05eb
Hansel <hansel.hir@gmail.com>
2020-07-29 10:55:20 -0500

    Style: Se hace salto de linea de la clase ErrorController

--- src/main/java/com/creativity/registrymicroservice/controller/ErrorController.java
+++ src/main/java/com/creativity/registrymicroservice/controller/ErrorController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class ErrorController {
 
     public static final String ISE_VIEW ="/error/500";
-
+    
     @ExceptionHandler(Exception.class)
     public String showInternalServerError(){
         return ISE_VIEW;
