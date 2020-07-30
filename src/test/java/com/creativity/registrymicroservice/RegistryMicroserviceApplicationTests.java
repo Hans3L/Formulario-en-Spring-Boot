e4289933ff1765e92f4468f54cf52c38cadef440
Hansel <hansel.hir@gmail.com>
2020-07-30 16:42:31 -0500

    feat: Clase principal del proyecto..

---
+++ src/test/java/com/creativity/registrymicroservice/RegistryMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.creativity.registrymicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
