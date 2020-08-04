1a1458b9300096c93dc92cc5a16e5f0c5f61de4a
Hansel <hansel.hir@gmail.com>
2020-08-04 16:59:46 -0500

    feat: Se agrega método de conversión de cifrado para prueba.

--- src/main/java/com/creativity/registrymicroservice/RegistryMicroserviceApplication.java
+++ src/main/java/com/creativity/registrymicroservice/RegistryMicroserviceApplication.java
@@ -2,12 +2,20 @@ package com.creativity.registrymicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class RegistryMicroserviceApplication {
 
 	public static void main(String[] args) {
+		lanzar();
 		SpringApplication.run(RegistryMicroserviceApplication.class, args);
+	}
+
+	private static void lanzar(){
+		BCryptPasswordEncoder pe = new BCryptPasswordEncoder();
+		System.out.println("PASSWORD CONVERTER");
+		System.out.println(pe.encode("user"));
 	}
 
 }
